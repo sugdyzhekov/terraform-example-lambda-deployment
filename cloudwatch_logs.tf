5ee538cb9744f97387fad593a1520a3f2506a597
Sergei Ugdyzhekov <sergey@ugdyzhekov.org>
2018-11-22 17:58:26 +0100

    Do not hardcode function name in CloudWatch Log group

--- cloudwatch_logs.tf
+++ cloudwatch_logs.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudwatch_log_group" "my_test_lambda" {
-  name              = "/aws/lambda/my_test_lambda_function"
+  name              = "/aws/lambda/${aws_lambda_function.test_lambda.function_name}"
   retention_in_days = 7
 }
